6c95ab4b497f6d9835fe9aa8ccedfa1ae2ebb2ed
rughione <ruggerospe@gmail.com>
2026-04-10 19:55:32 +0200

    Add match database and nearby airports data

---
+++ matches.js
@@ -0,0 +1,15 @@
+// Database delle partite e stadi
+const matchDatabase = [
+  {
+    teams: "Pescara - Sampdoria",
+    city: "Pescara",
+    airportCode: "PSR", // IATA code di Pescara
+    stadium: "Stadio Adriatico",
+    date: "2026-05-15" // Esempio
+  }
+];
+
+// Mappa degli aeroporti vicini a Genova (GOA)
+const nearbyAirports = {
+  "Genova": ["GOA", "MXP", "LIN", "TRN"] // Genova, Malpensa, Linate, Torino
+};
